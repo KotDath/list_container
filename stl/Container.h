b84997b40cb1d284b57d107f206ae56d56764a1f
KotDath <epidath@gmail.com>
2021-05-15 17:11:07 +0300

    Заготовка

--- stl/Container.h
+++ stl/Container.h
@@ -1,6 +1,42 @@
 #pragma once
+#include <utility>
 
+template<class KeyType, class DataType, class Hash>
+class UnorderedMap {
+public:
 
-// TODO: В данном файле должен быть реализован шаблонный класс контейнера.
-//       В отличие от обычных классов, реализацию шаблонных классов нельзя разбить на .h и .cpp файлы.
-//       Поэтому все методы реализуются непосредственно внутри шаблонного класса.
+private:
+    class List {
+    public:
+        using pairValue   = std::pair<KeyType, DataType>;
+        using pairPointer = pairValue*;
+
+    private:
+        struct Node {
+            Node() : value(), next(nullptr), prev(nullptr) {}
+            Node(const pairValue& value, pairPointer next, pairPointer prev) : value(value), next(next), prev(prev) {}
+            pairValue value;
+            pairPointer next;
+            pairPointer prev;
+        };
+        List();
+        explicit List(int count);
+        List(int count, pairValue defaultValue);
+        Node head;
+        Node tail;
+        int size;
+    };
+};
+
+template<class KeyType, class DataType, class Hash>
+UnorderedMap<KeyType, DataType, Hash>::List::List() : head(), tail(), size(0) {}
+
+template<class KeyType, class DataType, class Hash>
+UnorderedMap<KeyType, DataType, Hash>::List::List(int count) {
+
+}
+
+template<class KeyType, class DataType, class Hash>
+UnorderedMap<KeyType, DataType, Hash>::List::List(int count, UnorderedMap::List::pairValue defaultValue) {
+
+}
