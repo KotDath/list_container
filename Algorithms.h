db24adbf3188483b53d0a989fe669be6d3646da0
Илья Богданов <ibogdanov@bmstu.ru>
2021-06-29 13:03:08 +0000

    Замечания по Algorithms.h

--- Algorithms.h
+++ Algorithms.h
@@ -5,8 +5,8 @@
 
 template<class Iterator>
 concept Iterable = requires(Iterator first, Iterator second) {
-    { first == second} -> std::same_as<bool>;
-    { first != second} -> std::same_as<bool>;
+    { first == second} -> std::same_as<bool>;             //!!! is_convertible
+    { first != second} -> std::same_as<bool>;          //!!! is_convertible
     {*first} -> std::same_as<std::iter_reference_t<Iterator>>;
     *first = *second;
     first = second;
@@ -19,9 +19,16 @@ concept Iterable = requires(Iterator first, Iterator second) {
 
 template<class Iterator, class Predicate>
 concept ConceptPredicate = requires(Iterator it, Predicate predicate1, Predicate predicate2){
+    
+    //!!! Не очень понятно, какой смысл сравнивать bool с bool (true == true, false == true и т.д.)
     { predicate1(*it) == predicate2(*it)} -> std::same_as<bool>;
     { predicate1(*it) != predicate2(*it)} -> std::same_as<bool>;
-    {predicate1(*it)} -> std::same_as<bool>;
+    
+    
+    
+    
+    
+    {predicate1(*it)} -> std::same_as<bool>; //!!! is_convertible
 };
 
 template<class Iterator, class Function>
@@ -47,7 +54,7 @@ Iterator maxElement(Iterator beg, Iterator end, Function comp) {
     Iterator res = beg;
     ++beg;
     for(; beg != end; ++beg) {
-        if (comp(*beg) > comp(*res)) {
+        if (comp(*beg) > comp(*res)) { //!!! Неверно. Должен быть бинарный предикат (с проверкой на >)
             res = beg;
         }
     }
@@ -61,7 +68,7 @@ Iterator minElement(Iterator beg, Iterator end, Function comp) {
     Iterator res = beg;
     ++beg;
     for(; beg != end; ++beg) {
-        if (comp(*beg) < comp(*res)) {
+        if (comp(*beg) < comp(*res)) { //!!! Неверно. Должен быть бинарный предикат (с проверкой на <)
             res = beg;
         }
     }
